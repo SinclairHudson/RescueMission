3b8dcde4e86aa3887f1745a9018ae1af0e91e1d4
SinclairHudson <hudson.sinclair00@gmail.com>
2019-08-14 15:17:04 -0400

    Should Work Now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
+import Map from './components/Map.js';
 
 function App() {
   return (
     <div className="App">
-      <Form />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Map/>
         <p>
-          RESCUEMISSION
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
