42a694acf9d6af9c296d36b1c4da88006883547f
SinclairHudson <hudson.sinclair00@gmail.com>
2019-08-14 15:26:10 -0400

    Added map component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.js';
+import Form from './components/Form.js';
 
 function App() {
   return (
@@ -9,17 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Map/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form/>
       </header>
     </div>
   );
